6dc6d1addd766e82ae242e43507e5e5ae24a944f
rupesss-09 <rupesss09@gmail.com>
2022-04-07 22:35:51 -0400

    Creating statement and fetching data using Statement interface.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("Connection made to Database.");
         Connection con = connection.getConnectionToAwsDatabase();
 
-        String updateQuery = "insert into Products (PersonID, LastName, FirstName, Address, City) values ('2', 'Manan', 'Roshani', '140 Mati Rd', 'Charikot')";
+        /*String updateQuery = "insert into Products (PersonID, LastName, FirstName, Address, City) values ('2', 'Manan', 'Roshani', '140 Mati Rd', 'Charikot')";
         String query = "SELECT * FROM Products";
         Statement st = con.createStatement();
         ResultSet resultSet = st.executeQuery(query);
@@ -21,7 +21,17 @@ public class Main {
         String firstName = resultSet.getString("FirstName");
         System.out.println(firstName);
         int rows = st.executeUpdate(updateQuery);
-        System.out.println("Number of rows affected: " + rows );
+        System.out.println("Number of rows affected: " + rows );*/
+
+        //How to write query when data is in Variables
+        String tableName = "Products";
+        String query = "select * from " + tableName;
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        rs.next();
+        rs.next();
+        String firstName = rs.getString("FirstName");
+        System.out.println(firstName);
 
         /*
         We have 3 types of language in Sql
