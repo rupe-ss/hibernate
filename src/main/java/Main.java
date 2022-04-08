bf265f2f59ffeaf05dea7d93b76634906457cb75
rupesss-09 <rupesss09@gmail.com>
2022-04-07 22:29:32 -0400

    Statement and 3 Types of Sql languages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,12 +13,22 @@ public class Main {
             System.out.println("Connection made to Database.");
         Connection con = connection.getConnectionToAwsDatabase();
 
-//        String query = "insert into test.Products (PersonID, LastName, FirstName, Address, City) values (\"2\", \"Manan\", \"Roshani\", \"140 Mati Rd\", \"Charikot\");";
+        String updateQuery = "insert into Products (PersonID, LastName, FirstName, Address, City) values ('2', 'Manan', 'Roshani', '140 Mati Rd', 'Charikot')";
         String query = "SELECT * FROM Products";
         Statement st = con.createStatement();
         ResultSet resultSet = st.executeQuery(query);
         resultSet.next();
         String firstName = resultSet.getString("FirstName");
         System.out.println(firstName);
+        int rows = st.executeUpdate(updateQuery);
+        System.out.println("Number of rows affected: " + rows );
+
+        /*
+        We have 3 types of language in Sql
+        1. DDL - Data Definition Language => Creating Database table
+        2. DML - Data Manipulation Language => Updating, inserting and more.
+        3. DQL - Data Query Language => Fetching data from Database
+        4. TCL - Transition Control Language => Grant permission, revoke permission.
+         */
     }
 }
