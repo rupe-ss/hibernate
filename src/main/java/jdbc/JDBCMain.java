cb666731e838a5855dd4a13fe4dd76fb3a716c53
rupesss-09 <rupesss09@gmail.com>
2022-04-08 11:15:05 -0400

    Updating database through pojo class.

--- src/main/java/jdbc/JDBCMain.java
+++ src/main/java/jdbc/JDBCMain.java
@@ -5,8 +5,25 @@ import jdbc.dao.ProductDao;
 
 public class JDBCMain {
     public static void main(String[] args) throws Exception {
+        /*//Fetching data from database
         ProductDao productDao = new ProductDao();
         Product product = productDao.getProduct(1);
-        System.out.println(product.getFirstName());
+        System.out.println(product.getFirstName());*/
+
+        //Let's create a Object of class Product and Update data to database
+        Product product1 = new Product();
+        product1.setId(4);
+        product1.setFirstName("Chandra");
+        product1.setLastName("Jodaic");
+        product1.setAddress("123 Balbitar St");
+        product1.setCity("Chandani");
+
+        ProductDao productDao = new ProductDao();
+        boolean check = productDao.addProduct(product1);
+        if(check){
+            System.out.println("Database updated successfully.");
+        } else{
+            System.out.println("Something went wrong.");
+        }
     }
 }
--- src/main/java/jdbc/dao/ProductDao.java
+++ src/main/java/jdbc/dao/ProductDao.java
@@ -40,11 +40,32 @@ public class ProductDao {
             product.setFirstName(rs.getString("FirstName"));
             product.setLastName(rs.getString("LastName"));
         }catch (Exception e){
-
+            System.out.println("Something went while fetching data." + e.getStackTrace());
         }finally {
             connection.close();
             ps.close();
             return product;
+        }
+    }
+
+    public boolean addProduct(Product product1) throws Exception {
+        Connection connection = getConnection();
+        String query = "insert into Products values (?,?,?,?,?)";
+        int row = 0;
+        try{
+            PreparedStatement ps = getPreparedStatement(connection, query);
+            ps.setInt(1, product1.getId());
+            ps.setString(2, product1.getLastName());
+            ps.setString(3, product1.getFirstName());
+            ps.setString(4, product1.getAddress());
+            ps.setString(5, product1.getCity());
+            row = ps.executeUpdate();
+        }catch (Exception e){
+            System.out.println("Something went wrong while executing the update query" + e.getStackTrace());
+        }finally {
+            if(row > 0)
+                return true;
+            else return false;
         }
     }
 }
