36ca7a6cada4a2931f79483137daac00454f2e53
rupesss-09 <rupesss09@gmail.com>
2022-04-10 16:52:09 -0400

    Added HibernateMain class where all the hibernate execution code will exists plus the notes.

---
+++ src/main/java/hibernate/HibernateMain.java
@@ -0,0 +1,12 @@
+package hibernate;
+
+/*
+Add, dependency
+Here, we will create and add rows in Database Table.
+So, I have a Student class in this package. We will use this class as Entity class or pojo class.
+ */
+public class HibernateMain {
+    public static void main(String[] args) {
+
+    }
+}
