ad23a751b47c12eff8e4f2129ae230cae061f54f
rupesss-09 <rupesss09@gmail.com>
2022-04-10 16:51:05 -0400

    Created a student POJO class with getter, setter and toString.

---
+++ src/main/java/hibernate/Student.java
@@ -0,0 +1,40 @@
+package hibernate;
+
+public class Student {
+    int id;
+    String name;
+    double gpa;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
