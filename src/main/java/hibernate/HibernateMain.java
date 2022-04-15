1642fd3dc512496bdcb8a99bf51ad2e98c5bc8ab
rupesss-09 <rupesss09@gmail.com>
2022-04-15 09:52:25 -0400

    Fetching data from database with get() method.

--- src/main/java/hibernate/HibernateMain.java
+++ src/main/java/hibernate/HibernateMain.java
@@ -21,15 +21,23 @@ How to create configure file ?
 public class HibernateMain {
     static HibernateSetup hs = new HibernateSetup();
     public static void main(String[] args) {
-        addStudent(3, "Champa", 2.9);
+        //addStudent(3, "Champa", 2.9);
+        System.out.println(getStudent(1));
     }
+
     public static void addStudent(int id, String name, double gpa){
         Student student = new Student(id, name,gpa);
         hs.getSession().save(student);
         hs.getTransaction().commit();
     }
-}
 
+    public static Student getStudent(int id){
+        Student student = new Student();
+        student = hs.getSession().get(Student.class,id);
+        return student;
+    }
+
+}
 class HibernateSetup{
     //We need to provide configure file for Configuration Object, we need configure() method to do it.
     Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class);
